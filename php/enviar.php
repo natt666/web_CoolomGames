1c789939f5e1c179555dc3d500bdb49166a2a190
natt666 <nataaaliaia@gmail.com>
2026-02-01 16:05:23 +0100

    ywp

--- php/enviar.php
+++ php/enviar.php
@@ -1,31 +1,33 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nom = htmlspecialchars($_POST['name']);
-        $email = htmlspecialchars($_POST['email']);
-        $assumpte = htmlspecialchars($_POST['subject']);
-        $missatge = htmlspecialchars($_POST['message']);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $assumpte = htmlspecialchars($_POST['subject']);
+    $missatge = htmlspecialchars($_POST['message']);
 
-        $destinatari = "coolomgames@gmail.com"; 
+    $destinatari = "coolomgames@gmail.com"; 
 
-        $mail_assumpte = "Nou missatge des del formulari del web: " . $assumpte;
-
-        $cos = "Nom: $nom\n";
-        $cos .= "Correu electrònic: $email\n";
-        $cos .= "Assumpte: $assumpte\n\n";
-        $cos .= "Missatge:\n$missatge\n";
-
-        $headers = "From: $email\r\n";
-        $headers .= "Reply-To: $email\r\n";
+    $mail_assumpte = "Nou missatge web: " . $assumpte;
+    $cos = "Has rebut un nou missatge des del formulari de contacte:\n\n";
+    $cos .= "Nom: $nom\n";
+    $cos .= "Correu: $email\n";
+    $cos .= "Assumpte: $assumpte\n\n";
+    $cos .= "Missatge:\n$missatge\n";
+    $headers = "From: no-reply@coolomgames.com\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     if (mail($destinatari, $mail_assumpte, $cos, $headers)) {
-    header("Location: contact.html?status=success");
-            exit();
-        } else {
-            header("Location: contact.html?status=error");
-            exit();
-        }
+        header("Location: /contact.html?status=success");
+        exit();
     } else {
-        header("Location: contact.html");
+        header("Location: /contact.html?status=error");
         exit();
     }
+} else {
+    header("Location: /contact.html");
+    exit();
+}
 ?>
